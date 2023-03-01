4efcf3522371591b7eb56048c53f639c44557b84
Ruizhe Zhong <zhongruizhe271828@foxmail.com>
2023-03-01 17:20:21 +0800

    update: add Config class

---
+++ config.py
@@ -0,0 +1,29 @@
+import utils
+
+class Config:
+    @staticmethod
+    def _save(path:str):
+        args = {}
+        for k in dir(Config):
+            if not k.startswith("_"):
+                args[k] = getattr(Config, k)
+        utils.save_json(args, path)
+
+    @staticmethod
+    def _display(n:int=60):
+        print("*"*n)
+        for k in dir(Config):
+            if not k.startswith("_"):
+                print("{:<20}\t{}".format(k, getattr(Config, k)))
+        print("*"*n)
+
+    @staticmethod
+    def _load(path:str):
+        args = utils.load_json(path)
+        for k,v in args.items():
+            setattr(Config, k, v)
+    
+    input_dim = 100000
+    hidden_dim = 100000
+    output_dim = 100000
+    num_layers = 100000
